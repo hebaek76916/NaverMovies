32cb552f216f17b358333c35a4639cd75fdbc955
hebaek76916 <qorgusdms15@gmail.com>
2022-06-13 22:40:19 +0900

    tableView Set

--- NaverMovies/NaverMovies/ViewController.swift
+++ NaverMovies/NaverMovies/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(MovieInfoTableViewCell.self,
+                           forCellReuseIdentifier: MovieInfoTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpTableView()
+        setUpLayout()
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate,
+                          UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieInfoTableViewCell.identifier,
+                                                       for: indexPath) as? MovieInfoTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
+}
+
+extension ViewController {
+    private func setUpTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func setUpLayout() {
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+    }
+}
