a42968f805bb6fe385a9d09b26cbaca2e9a9a68a
hebaek76916 <qorgusdms15@gmail.com>
2022-06-16 22:26:01 +0900

    Extensions set

---
+++ NaverMovies/NaverMovies/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  NaverMovies
+//
+//  Created by 현은백 on 2022/06/15.
+//
+
+import UIKit
+import Kingfisher
+
+extension String {
+   func removeHTMLTag() -> String {
+       return self.replacingOccurrences(of: "<[^>]+>", with: "", options: String.CompareOptions.regularExpression, range: nil)
+    }
+}
+
+extension UIImageView {
+    func setImage(with source: String?) {
+        guard let source = source else { return }
+        guard let url = URL(string: source) else { return }
+        self.kf.setImage(with: url,
+                         placeholder: nil,
+                         options: [
+                             .loadDiskFileSynchronously,
+                             .cacheOriginalImage,
+                             .transition(.fade(0.25))
+                         ],
+                         completionHandler: { result in
+            
+            
+        })
+    
+    }
+}
