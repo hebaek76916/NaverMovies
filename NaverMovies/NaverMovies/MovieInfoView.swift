454f0cae9f6cae63f01891df821a379eeb0cc375
hebaek76916 <qorgusdms15@gmail.com>
2022-06-18 15:37:28 +0900

    set Movie info View with Layout and Tap gesture action

--- NaverMovies/NaverMovies/MovieInfoView.swift
+++ NaverMovies/NaverMovies/MovieInfoView.swift
@@ -6,9 +6,18 @@
 //
 
 import UIKit
-import Kingfisher
 
 class MovieInfoView: UIView {
+    
+    private var isStarSelected = false
+    private var model: MovieInfo?
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     private let thumbnailView: UIImageView = {
         let imageView = UIImageView()
@@ -62,6 +71,7 @@ class MovieInfoView: UIView {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "heart.fill")
         imageView.isUserInteractionEnabled = true
+        imageView.tintColor = .gray
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -69,6 +79,7 @@ class MovieInfoView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setUpLayout()
+        addGestureStarMark()
     }
     
     required init?(coder: NSCoder) {
@@ -76,18 +87,74 @@ class MovieInfoView: UIView {
     }
     
     public func updateUI(model: MovieInfo) {
+        self.model = model
         thumbnailView.setImage(with: model.image)
-        
         titleLabel.text = String(format: "제목 : %@", model.title?.removeHTMLTag() ?? "")
         directorLabel.text = String(format: "감독 : %@", model.director ?? "")
         castLabel.text = String(format: "출연진 : %@", model.actor ?? "")
         scoreLabel.text = String(format: "평점 : %@", model.userRating ?? "")
+        setStarMark()
+    }
+    
+    public func reuse() {
+        thumbnailView.image = nil
+        titleLabel.text = nil
+        directorLabel.text = nil
+        castLabel.text = nil
+        scoreLabel.text = nil
+        setStarMark()
     }
 }
 
 extension MovieInfoView {
     
+    private func addGestureStarMark() {
+        let tap = UITapGestureRecognizer(target: self,
+                                         action: #selector(tabStar))
+        starMark.addGestureRecognizer(tap)
+    }
+    
+    private func setStarMark() {
+        guard let model = model else { return }
+        let favorites = FavoritesManager.shared.getFavoritesData()
+        
+        if favorites.contains(where: { $0 == model }) {
+            starMark.tintColor = .yellow
+        } else {
+            starMark.tintColor = .gray
+        }
+    }
+    
+    @objc func tabStar(_ sender: UITapGestureRecognizer) {
+        
+        guard let model = self.model else { return }
+        
+        let savedFavorites = FavoritesManager.shared.getFavoritesData()
+        if savedFavorites.contains(where: { $0 == model }) {
+            isStarSelected = true
+        }
+
+        isStarSelected = !isStarSelected
+        
+        if isStarSelected {
+            starMark.tintColor = .yellow
+            FavoritesManager.shared.add(model: model)
+            
+        } else {
+            starMark.tintColor = .gray
+            FavoritesManager.shared.minus(model: model)
+        }
+    }
+    
     private func setUpLayout() {
+        addSubview(contentView)
+        [
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ].forEach{ $0.isActive = true }
+        
         // Labels Layout
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -102,19 +169,19 @@ extension MovieInfoView {
             scoreLabel
         ].forEach{ stackView.addArrangedSubview($0) }
         
-        addSubview(stackView)
+        contentView.addSubview(stackView)
         [
-            stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ].forEach{ $0.isActive = true }
         
         // Thumbnail Image Image View Layout
-        addSubview(thumbnailView)
+        contentView.addSubview(thumbnailView)
         [
             thumbnailView.topAnchor.constraint(equalTo: stackView.topAnchor),
-            thumbnailView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            thumbnailView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             thumbnailView.bottomAnchor.constraint(equalTo: stackView.bottomAnchor),
-            thumbnailView.widthAnchor.constraint(equalTo: heightAnchor,
+            thumbnailView.widthAnchor.constraint(equalTo: stackView.heightAnchor,
                                                  multiplier: 1/1.4)
         ].forEach{ $0.isActive = true }
         
@@ -122,40 +189,13 @@ extension MovieInfoView {
         stackView.leadingAnchor.constraint(equalTo: thumbnailView.trailingAnchor,
                                            constant: 10).isActive = true
         // Star Mark Layout
-        starMark.backgroundColor = .yellow
-        addSubview(starMark)
+        contentView.addSubview(starMark)
         [
-            starMark.trailingAnchor.constraint(equalTo: trailingAnchor),
-            starMark.topAnchor.constraint(equalTo: topAnchor),
+            starMark.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            starMark.topAnchor.constraint(equalTo: contentView.topAnchor),
             starMark.widthAnchor.constraint(equalToConstant: 32),
             starMark.heightAnchor.constraint(equalToConstant: 32)
         ].forEach{ $0.isActive = true }
         
-    }
-    
-}
-
-extension String {
-   func removeHTMLTag() -> String {
-       return self.replacingOccurrences(of: "<[^>]+>", with: "", options: String.CompareOptions.regularExpression, range: nil)
-    }
-}
-
-extension UIImageView {
-    func setImage(with source: String?) {
-        guard let source = source else { return }
-        guard let url = URL(string: source) else { return }
-        self.kf.setImage(with: url,
-                         placeholder: nil,
-                         options: [
-                             .loadDiskFileSynchronously,
-                             .cacheOriginalImage,
-                             .transition(.fade(0.25))
-                         ],
-                         completionHandler: { result in
-            
-            
-        })
-    
     }
 }
