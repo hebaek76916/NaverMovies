d40c51ff3fb589fd80b47b8f7149b71eb5459239
hebaek76916 <qorgusdms15@gmail.com>
2022-06-19 10:27:35 +0900

    ViewController using UISearchBar not UISearchController

--- NaverMovies/NaverMovies/ViewController.swift
+++ NaverMovies/NaverMovies/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     private var searchTimer: Timer?
+    private var results: [MovieInfo] = []
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -25,21 +26,27 @@ class ViewController: UIViewController {
         return searchBar
     }()
     
-    private var result: [MovieInfo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        setUpSearchController()
+        setUpUI()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
     }
 
+    @objc func moveToFavorites() {
+        let vc = FavoritesViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
-extension ViewController: UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-     
-        guard let query = searchController.searchBar.text,
-              let resultVC = searchController.searchResultsController as? SearchResultController,
+extension ViewController: UISearchBarDelegate {
+
+    func searchBar(_ searchBar: UISearchBar,
+                   textDidChange searchText: String) {
+        guard let query = searchBar.text,
               !(query.trimmingCharacters(in: .whitespaces).isEmpty) else {
                   return
               }
@@ -51,17 +58,62 @@ extension ViewController: UISearchResultsUpdating {
                                            block: { _ in
             
             APICaller.search(query: query) { result in
-                
                 guard let result = result else {
                     return
                 }
-                
+                print(result)
                 DispatchQueue.main.async {
-                    resultVC.update(with: result.items ?? [])
+                    self.results = result.items ?? []
+                    self.tableView.reloadData()
                 }
-
             }
         })
+    }
+}
+
+extension ViewController: UITableViewDelegate,
+                          UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieInfoTableViewCell.identifier,
+                                                       for: indexPath) as? MovieInfoTableViewCell else {
+            return UITableViewCell()
+        }
+        let model = results[indexPath.row]
+        cell.updateUI(model: model)
+        return cell
+    }
+}
+extension ViewController {
+    func setUpUI() {
+        view.backgroundColor = .systemBackground
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar)
+        [
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor,
+                                          constant: 68),
+            searchBar.heightAnchor.constraint(equalToConstant: 80)
+        ].forEach{ $0.isActive = true }
+        
+        view.addSubview(tableView)
+        [
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ].forEach{ $0.isActive = true }
+        
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks,
+                                                 target: self,
+                                                 action: #selector(moveToFavorites))
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
     }
 }
 
@@ -69,16 +121,4 @@ extension ViewController: SearchResultControllerDelegate {
     func searchResultViewController(searchResult: MovieResult) {
         
     }
-}
-
-extension ViewController {
-    
-    private func setUpSearchController() {
-        let resultVC = SearchResultController()
-        resultVC.delegate = self
-        let searchVC = UISearchController(searchResultsController: resultVC)
-        searchVC.searchResultsUpdater = self
-        navigationItem.searchController = searchVC
-    }
-    
 }
