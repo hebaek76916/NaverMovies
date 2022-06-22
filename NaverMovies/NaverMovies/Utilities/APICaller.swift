05e23b48a0972f76f441ff17c80aac4a8cf499e0
hebaek76916 <qorgusdms15@gmail.com>
2022-06-22 21:15:29 +0900

    API get 의 기능을 하는 request 함수를 생성.

--- NaverMovies/NaverMovies/Utilities/APICaller.swift
+++ NaverMovies/NaverMovies/Utilities/APICaller.swift
@@ -7,36 +7,90 @@
 
 import Foundation
 import Alamofire
-//Client ID
-//Urb56VLFsg_h6NK_ePnV
-//Client Secret
-//cT31L7Xx3i
 
 class APICaller {
     
+    static let shared = APICaller()
+    
     static let baseUrl: String = "https://openapi.naver.com/v1/search/movie.json"
+    
+    var isPaginating = false
+    
+    private var start = 1
+    
+    private static var params: Parameters = [:]
     
 }
 
 extension APICaller {
     
-    class func search(
-        query: String,
-        completion: @escaping (MovieResult?) -> Void
+    private func request<T: Codable>(
+        query: [String: Any],
+        expecting: T.Type,
+        completion: @escaping (Result<T, AFError>) -> Void
     ) {
-        
         let headers: HTTPHeaders = [
             "X-Naver-Client-Id": "Urb56VLFsg_h6NK_ePnV",
             "X-Naver-Client-Secret": "cT31L7Xx3i"
         ]
         
-        AF.request(baseUrl,
+        AF.request(APICaller.baseUrl,
                    method: .get,
-                   parameters: ["query": query],
+                   parameters: query,
                    headers: headers)
             .validate(statusCode: 200..<300)
-            .responseDecodable(of: MovieResult.self) { result in
-                completion(result.value)
+            .responseDecodable(of: T.self) { res in
+                completion(res.result)
             }
+    }
+    
+    
+    func search(
+        query: String,
+        completion: @escaping (MovieResult?) -> Void
+    ) {
+        APICaller.params = ["query": query,
+                            "display": 100,
+                            "start": 1]
+        
+        request(query: APICaller.params,
+                expecting: MovieResult.self) { res in
+            switch res {
+            case .success(let res):
+                completion(res)
+            case .failure:
+                completion(nil)
+            }
+        }
+    }
+    
+    func fetchData(
+        pagination: Bool = false,
+        completion: @escaping ([MovieInfo]) -> Void
+    ) {
+        if pagination {
+            isPaginating = true
+        }
+        
+        DispatchQueue.global().async {
+            guard let temp = APICaller.params["start"] as? Int else { return }
+            let start = temp + 100
+            APICaller.params["start"] = start
+            
+            self.request(query: APICaller.params,
+                    expecting: MovieResult.self) { res in
+                switch res {
+                case .success(let res):
+                    completion(res.items ?? [])
+                case .failure:
+                    completion([])
+                }
+                
+                if self.isPaginating {
+                    self.isPaginating = false
+                }
+            }
+        }
+        
     }
 }
